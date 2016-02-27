4cd08b550dbc3643709fa4d6c1b0173d4834dac1
SAUNIER DEBES Brice <brice.saunier@gmail.com>
2016-02-27 12:11:21 +0100

    Selection des lignes / colonnes à modifier

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+// --------------------------- main() method ---------------------------
+
+  public static void main(String[] args) {
+    LinearSystem ls = new LinearSystem();
+    ls.printTab();
+
+    Simplex simplex = new Simplex(ls);
+    System.out.println("\n");
+    simplex.getStep(0).printTab();
+    System.out.println();
+    simplex.solveLinearProblem();
+  }
 }
