9dd0d9627d064a51277a024bc1b177a64a988c49
SAUNIER DEBES Brice <brice.saunier@gmail.com>
2016-03-02 14:16:21 +0100

    Utilisation de Bigdecimal à la place des double

--- src/LinearSystem.java
+++ src/LinearSystem.java
@@ -5,61 +5,63 @@
  */
 
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class LinearSystem {
 
 // ------------------------------ FIELDS ------------------------------
 
-  private double[]   ecoFunction;
-  private double[][] constraints;
-  private int        nbrVariables;
+  private BigDecimal[]   ecoFunction;
+  private BigDecimal[][] constraints;
+  private int            nbrVariables;
 
 // --------------------------- CONSTRUCTORS ---------------------------
 
   public LinearSystem() {
-    double[] partialEcoFunction = askEcoFunction();
+    BigDecimal[] partialEcoFunction = askEcoFunction();
     makeConstraints(askConstraints());
     makeEcoFunction(partialEcoFunction);
   }
 
-  private double[] askEcoFunction() {
-    Scanner  sc = new Scanner(System.in);
-    double[] partialEcoFunction;
+  private BigDecimal[] askEcoFunction() {
+    Scanner      sc = new Scanner(System.in);
+    BigDecimal[] partialEcoFunction;
 
     System.out.println("Entrez le nombre de variables : ");
     nbrVariables = sc.nextInt();
-    partialEcoFunction = new double[nbrVariables + 1];
+    partialEcoFunction = new BigDecimal[nbrVariables + 1];
 
     System.out.println("\nFonction économique : ");
 
-    int    i = 0;
-    double valeur;
+    int        i = 0;
+    BigDecimal valeur;
     while (i < nbrVariables) {
       System.out.print("x" + (i + 1) + " : ");
-      valeur = sc.nextDouble();
+      valeur = sc.nextBigDecimal();
       partialEcoFunction[i] = valeur;
       i++;
     }
-    partialEcoFunction[i] = 0;
+    partialEcoFunction[i] = BigDecimal.ZERO;
     return partialEcoFunction;
   }
 
-  private void makeConstraints(double[][] partialConstraints) {
+  private void makeConstraints(BigDecimal[][] partialConstraints) {
     final int nbConstraints     = partialConstraints.length;
     final int constraintsLength = partialConstraints[0].length + nbConstraints;
     final int nbVariables       = partialConstraints[0].length - 1;
-    constraints = new double[nbConstraints][constraintsLength];
+    constraints = new BigDecimal[nbConstraints][constraintsLength];
 
     for (int i = 0; i < nbConstraints; i++) {
       for (int j = 0; j < constraintsLength; j++) {
         if (j >= nbVariables)//si variables déjà mis dans le tableau, on met une base
-          constraints[i][j] = isInDiagonal(nbVariables, i, j) ? 1 : 0;
+          constraints[i][j] = isInDiagonal(nbVariables, i, j) ? BigDecimal.ONE : BigDecimal.ZERO;
         else//sinon la variable est mise dans la contrainte finale
           constraints[i][j] = partialConstraints[i][j];
       }
       constraints[i][getConstraintsLength() - 1] =
-          partialConstraints[0][partialConstraints[i].length - 1];
+          partialConstraints[i][partialConstraints[i].length - 1];
     }
   }
 
@@ -69,42 +71,76 @@ public class LinearSystem {
 
   private int getConstraintsLength() {return constraints[0].length;}
 
-  private double[][] askConstraints() {
-    Scanner    sc = new Scanner(System.in);
-    double[][] partialConstraints;
-    double     variable;
-    int        nbrConstraints;
-    int        i, j;
+  private BigDecimal[][] askConstraints() {
+    Scanner        sc = new Scanner(System.in);
+    BigDecimal[][] partialConstraints;
+    BigDecimal     variable;
+    int            nbrConstraints;
+    int            i, j;
 
     System.out.print("\nNombre de contraintes : ");
     nbrConstraints = sc.nextInt();
-    partialConstraints = new double[nbrConstraints][];
+    partialConstraints = new BigDecimal[nbrConstraints][];
 
     for (i = 0; i < nbrConstraints; i++) {
-      partialConstraints[i] = new double[nbrVariables + 1];
+      partialConstraints[i] = new BigDecimal[nbrVariables + 1];
       for (j = 0; j < nbrVariables; j++) {
         System.out.print("x" + (j + 1) + " : ");
-        variable = sc.nextInt();
+        variable = sc.nextBigDecimal();
         partialConstraints[i][j] = variable;
       }
       System.out.print("Valeur : ");
-      variable = sc.nextInt();
+      variable = sc.nextBigDecimal();
       partialConstraints[i][j] = variable;
       System.out.println();
     }
     return partialConstraints;
   }
 
-  private void makeEcoFunction(double[] partialEcoFunction) {
-    this.ecoFunction = new double[getConstraintsLength()];
+  private void makeEcoFunction(BigDecimal[] partialEcoFunction) {
+    this.ecoFunction = new BigDecimal[getConstraintsLength()];
 
     for (int i = 0; i < ecoFunction.length; i++)
-      ecoFunction[i] = i < partialEcoFunction.length - 1 ? partialEcoFunction[i] : 0;
+      ecoFunction[i] = i < partialEcoFunction.length - 1 ? partialEcoFunction[i] : BigDecimal.ZERO;
 
     ecoFunction[ecoFunction.length - 1] = partialEcoFunction[partialEcoFunction.length - 1];
   }
 
+  public LinearSystem(LinearSystem linearSystem) {
+    this.ecoFunction = linearSystem.getEcoFunction().clone();
+    this.nbrVariables = linearSystem.getNbrVariables();
+    this.constraints = new BigDecimal[linearSystem.getConstraints().length][];
+    for (int i = 0; i < this.constraints.length; i++)
+      this.constraints[i] = linearSystem.getConstraints()[i].clone();
+  }
+
+  public LinearSystem(BigDecimal[] ecoFunction, BigDecimal[][] constraints, int nbrVariables) {
+    this.ecoFunction = ecoFunction;
+    this.constraints = constraints;
+    this.nbrVariables = nbrVariables;
+  }
+
 // -------------------------- OTHER METHODS --------------------------
+
+  public BigDecimal[][] getConstraints() {
+    return constraints;
+  }
+
+  public void setConstraints(BigDecimal[][] constraints) {
+    this.constraints = constraints;
+  }
+
+  public BigDecimal[] getEcoFunction() {
+    return ecoFunction;
+  }
+
+  public void setEcoFunction(BigDecimal[] ecoFunction) {
+    this.ecoFunction = ecoFunction;
+  }
+
+  public int getNbrVariables() {
+    return nbrVariables;
+  }
 
   public void printTab() {
     System.out.println(this.toString());
@@ -132,36 +168,20 @@ public class LinearSystem {
 
   private String getEcoFunctionLineAsString() {
     String str = "";
-    for (double variable : ecoFunction)
-      str += variable + "\t\t";
+    for (BigDecimal variable : ecoFunction)
+      str += variable.setScale(2, RoundingMode.HALF_EVEN).toPlainString() + "\t\t";
     str += "\n";
     return str;
   }
 
   private String getConstraintsLinesAsString() {
     String str = "";
-    for (double[] constraint : constraints) {
-      for (double variable : constraint)
-        str += variable + "\t\t";
+    for (BigDecimal[] constraint : constraints) {
+      for (BigDecimal variable : constraint)
+        str += variable.setScale(2, RoundingMode.HALF_EVEN).toPlainString() + "\t\t";
       str += "\n";
     }
     str += "\n";
     return str;
-  }
-
-  public double[] getEcoFunction() {
-    return ecoFunction;
-  }
-
-  public void setEcoFunction(double[] ecoFunction) {
-    this.ecoFunction = ecoFunction;
-  }
-
-  public double[][] getConstraints() {
-    return constraints;
-  }
-
-  public void setConstraints(double[][] constraints) {
-    this.constraints = constraints;
   }
 }
--- src/Simplex.java
+++ src/Simplex.java
@@ -5,6 +5,9 @@
  */
 
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +15,10 @@ public class Simplex {
 
 // ------------------------------ FIELDS ------------------------------
 
-  List<LinearSystem> system;
+  private static RoundingMode ROUND_EVEN  = RoundingMode.HALF_EVEN;
+  private static int          ROUND_SCALE = 15;
+  private static MathContext  PRECISION   = new MathContext(15);
+  private List<LinearSystem> system;
 
 // --------------------------- CONSTRUCTORS ---------------------------
 
@@ -23,19 +29,19 @@ public class Simplex {
 
 // -------------------------- OTHER METHODS --------------------------
 
-  private LinearSystem getCurrentStep() {
-    return system.get(system.size() - 1);
-  }
-
   private int getIndexOfColumnToPutIn() {
-    int            indexOfColumnToPutIn = 0;
-    final double[] ecoFunction          = getCurrentStep().getEcoFunction();
+    int                indexOfColumnToPutIn = 0;
+    final BigDecimal[] ecoFunction          = getCurrentStep().getEcoFunction();
 
     for (int i = 0; i < ecoFunction.length - 1; i++) {
-      if (ecoFunction[i] > ecoFunction[indexOfColumnToPutIn])
+      if (isLowerThan(ecoFunction[indexOfColumnToPutIn], ecoFunction[i]))
         indexOfColumnToPutIn = i;
     }
     return indexOfColumnToPutIn;
+  }
+
+  private boolean isLowerThan(BigDecimal value, BigDecimal valueToCompare) {
+    return value.compareTo(valueToCompare) == -1;
   }
 
   public LinearSystem getStep(int step) {
@@ -48,61 +54,113 @@ public class Simplex {
   }
 
   private boolean isOptimalSolution() {
-    final double[] ecoFunction = getCurrentStep().getEcoFunction();
+    final BigDecimal[] ecoFunction = getCurrentStep().getEcoFunction();
 
     for (int i = 0; i < ecoFunction.length - 1; i++) {
-      if (ecoFunction[i] > 0)
+      if (isGreaterThan(ecoFunction[i], BigDecimal.ZERO))
         return false;
     }
     return true;
   }
 
+  private boolean isGreaterThan(BigDecimal value, BigDecimal valueToCompare) {
+    return value.compareTo(valueToCompare) == 1;
+  }
+
   private void computeNextStep() {
     final int indexOfColumnToPutIn = getIndexOfColumnToPutIn();
     final int indexOfLineToExtract = getIndexOfLineToExtract(indexOfColumnToPutIn);
+    System.out.println(
+        "Step : " + getCurrentStepNumber() + ", line to extract : " + indexOfLineToExtract
+            + ", column to put in : " + indexOfColumnToPutIn);
 
-    System.out.println("Step : " + getCurrentStepNumber() + ", line to extract : " + indexOfLineToExtract + ", column to put in : " + indexOfColumnToPutIn);
+    System.out.println(getCurrentStep());
+    LinearSystem currentStep = getCurrentStep();
+    LinearSystem nextStep    = new LinearSystem(getCurrentStep());
 
+    BigDecimal coefficient = BigDecimal.ONE
+        .divide(currentStep.getConstraints()[indexOfLineToExtract][indexOfColumnToPutIn],
+            ROUND_SCALE, ROUND_EVEN);
 
+    for (int i = 0; i < nextStep.getEcoFunction().length; i++) {
+      nextStep.getEcoFunction()[i] = nextStep.getEcoFunction()[i].subtract(
+          coefficient.multiply(currentStep.getEcoFunction()[indexOfColumnToPutIn], PRECISION)
+              .multiply(currentStep.getConstraints()[indexOfLineToExtract][i], PRECISION),
+          PRECISION);
+    }
+
+    for (int i = 0; i < currentStep.getConstraints().length; i++) {
+      for (int j = 0; j < currentStep.getConstraints()[0].length; j++) {
+        if (i != indexOfLineToExtract)
+          nextStep.getConstraints()[i][j] = nextStep.getConstraints()[i][j].subtract(
+              coefficient.multiply(currentStep.getConstraints()[i][indexOfColumnToPutIn], PRECISION)
+                  .multiply(currentStep.getConstraints()[indexOfLineToExtract][j], PRECISION),
+              PRECISION);
+        else
+          nextStep.getConstraints()[i][j] = nextStep.getConstraints()[i][j]
+              .divide(currentStep.getConstraints()[indexOfLineToExtract][indexOfColumnToPutIn],
+                  ROUND_SCALE, ROUND_EVEN);
+      }
+    }
+    System.out.println(nextStep);
+    this.system.add(nextStep);
 
   }
 
   private int getIndexOfLineToExtract(int indexOfColumnToPutIn) {
-    int              initialIndexOfLineToExtract;
-    final double[][] constraints          = getCurrentStep().getConstraints();
+    int                  initialIndexOfLineToExtract;
+    final BigDecimal[][] constraints = getCurrentStep().getConstraints();
 
     initialIndexOfLineToExtract = initialiseIndexOfLineToExtract(constraints, indexOfColumnToPutIn);
 
     return getSmallestRatiosLine(constraints, indexOfColumnToPutIn, initialIndexOfLineToExtract);
   }
 
-  private int initialiseIndexOfLineToExtract(double[][] constraints, int indexOfColumnToPutIn) {
+  private int initialiseIndexOfLineToExtract(BigDecimal[][] constraints, int indexOfColumnToPutIn) {
     int initialIndexOfLineToExtract = 0;
 
-    while (getRatio(constraints, initialIndexOfLineToExtract, indexOfColumnToPutIn) <= 0)
+    while (isLowerOrEqualTo(
+        getRatio(constraints, initialIndexOfLineToExtract, indexOfColumnToPutIn), BigDecimal.ZERO))
       initialIndexOfLineToExtract++;
 
     return initialIndexOfLineToExtract;
   }
 
-  private double getRatio(double[][] constraints, int line, int column) {
-    return constraints[line][column] / constraints[line][constraints[0].length-1];
+  private boolean isLowerOrEqualTo(BigDecimal variable, BigDecimal variableToCompare) {
+    return variable.compareTo(variableToCompare) == -1
+        || variable.compareTo(variableToCompare) == 0;
   }
 
-  private int getSmallestRatiosLine(double[][] constraints, int indexOfColumnToPutIn, int indexOfLineToExtract) {
-    final int nbLines = constraints.length;
-    double newRatio, oldRatio;
+  private BigDecimal getRatio(BigDecimal[][] constraints, int line, int column) {
+    return constraints[line][constraints[0].length - 1]
+        .divide(constraints[line][column], ROUND_SCALE, ROUND_EVEN);
+  }
+
+  private int getSmallestRatiosLine(BigDecimal[][] constraints, int indexOfColumnToPutIn,
+      int indexOfLineToExtract) {
+    final int  nbLines = constraints.length;
+    BigDecimal newRatio, oldRatio;
 
     for (int i = 0; i < nbLines; i++) {
-      newRatio = getRatio(constraints, i, indexOfColumnToPutIn);
-      oldRatio = getRatio(constraints, indexOfLineToExtract, indexOfColumnToPutIn);
-      if (newRatio < oldRatio && newRatio > 0)
-        indexOfLineToExtract = i;
+      if (isDifferentOfZero(constraints[i][indexOfColumnToPutIn])) {
+        newRatio = getRatio(constraints, i, indexOfColumnToPutIn);
+        oldRatio = getRatio(constraints, indexOfLineToExtract, indexOfColumnToPutIn);
+        if (isLowerThan(newRatio, oldRatio) && isGreaterThan(newRatio, BigDecimal.ZERO))
+          indexOfLineToExtract = i;
+      }
     }
     return indexOfLineToExtract;
   }
 
+  private boolean isDifferentOfZero(BigDecimal variable) {
+    return variable.compareTo(BigDecimal.ZERO) != 0;
+  }
+
   private int getCurrentStepNumber() {
     return system.size() - 1;
+  }
+
+  private LinearSystem getCurrentStep() {
+    return system.get(system.size() - 1);
   }
 }
